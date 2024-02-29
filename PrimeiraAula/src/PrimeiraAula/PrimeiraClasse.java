ccfad77d0c0a236e58b4bccc6db38ac72d969fed
Gustavo Neves Braun - 322130649 <322130649@ulife.com.br>
2024-02-29 10:38:04 -0300

    Atualização.

--- PrimeiraAula/src/PrimeiraAula/PrimeiraClasse.java
+++ PrimeiraAula/src/PrimeiraAula/PrimeiraClasse.java
@@ -1,6 +1,9 @@
 package PrimeiraAula;
 
 public class PrimeiraClasse {
-  int num1 = 3, num2 = 5, soma;
-  soma = num1 + num2;
+  public static void main(String[] args) {
+    int num1 = 3, num2 = 5, soma;
+    soma = num1 + num2;
+    System.out.println(soma);
+  }
 }
